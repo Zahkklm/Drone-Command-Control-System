77b20d5dddae2fd0964bf7865e47440891bf4f15
Özgür Peynirci <ozgurpeynirci@gmail.com>
2025-06-20 15:17:13 +0300

    feat(auth): add custom JWT serializer and update requirements

--- authentication_service/authentication_service/auth_api.py
+++ authentication_service/authentication_service/auth_api.py
@@ -1,10 +1,12 @@
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+from rest_framework_simplejwt.views import TokenRefreshView
+from users.serializers import CustomTokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
 from django.urls import path
 
+class CustomTokenView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
+
 urlpatterns = [
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/', CustomTokenView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
